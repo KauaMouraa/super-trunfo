a814fdc2370287e314c9d49f8495b1679697ac84
KauaMouraa <kauamoura.dev@gmail.com>
2025-04-23 16:43:35 -0300

    add cards var

--- Super Trunfo/main.c
+++ Super Trunfo/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    
+
+    // Dados da primeira carta
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos1;
+
+    // Dados da segunda carta
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos2;
+
 }
