9963a04c6e3dc9f83cbb2b48f7f323e0e03d0a4f
KauaMouraa <kauamoura.dev@gmail.com>
2025-04-23 16:55:44 -0300

    add cards inputs

--- Super Trunfo/main.c
+++ Super Trunfo/main.c
@@ -20,4 +20,62 @@ int main() {
     float pib2;
     int pontos2;
 
+    // Entrada da primeira carta
+    printf("---- Carta 1 ----\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("---- Nome da cidade ----\n");
+    printf("Se houver espaço entre o nome da cidade, coloque um ''_'' ao inves de espaço \n");
+    printf("Exemplo: Rio_de_Janeiro \n");
+    printf("Nome da Cidade: \n");
+    scanf("%s", cidade1);
+
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Nº de Pontos Turísticos: ");
+    scanf("%d", &pontos1);
+
+    // Entrada da segunda carta
+    
+    printf("\n---- Carta 2 ----\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo2);
+
+    printf("---- Nome da cidade ----\n");
+    printf("Se houver espaço entre o nome da cidade, coloque um ''_'' ao inves de espaço \n");
+    printf("Exemplo: Rio_de_Janeiro \n");
+    printf("Nome da Cidade: \n");
+    scanf("%s", cidade2);
+
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Nº de Pontos Turísticos: ");
+    scanf("%d", &pontos2);
+
+    return 0;
 }
